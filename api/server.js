26aca68f74f8de61ff4ef2a85c33f9f57741a928
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-26 14:48:03 -0400

    Updates name of 'router' folder to 'lambdaData' and changes nested file's name from 'router.js' to 'lambdaDataRouter.js'

--- api/server.js
+++ api/server.js
@@ -1,27 +1,13 @@
 const express = require('express');
 const helmet = require('helmet');
 
-const knex = require('knex'); // Step 1: Bring in the knexjs library
+const lambdaDataRouter = require('../lambdaData/lambdaDataRouter.js');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
-const knexConfig = {
-  // Step 2: Configure the knexjs library to connect to 
-  // './data/lambda.sqlite3' using sqlite3 module
-  client: "sqlite3", // this is the driver
-  useNullAsDefault: true,
-  connection: {
-    filename: "./data/lambda.sqlite3"
-  }
-  // debug: true
-};
-
-const server = express();
-
-server.use(express.json());
-server.use(helmet());
+server.use('/api/lambdaData', lambdaDataRouter);
 
 module.exports = server;
---
+++ lambdaData/lambdaDataRouter.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const knex = require("knex"); // Step 1: Bring in the knexjs library
+
+const knexConfig = {
+    // Step 2: Configure the knexjs library to connect to 
+    // './data/lambda.sqlite3' using sqlite3 module
+    client: "sqlite3", // this is the driver
+    useNullAsDefault: true,
+    connection: {
+      filename: "./data/lambda.sqlite3"
+    },
+    // debug: true
+};
+
+const db = knex(knexConfig); // Step 3: Set the 'knex(knexConfig)' to a variable called 'db'
+
+router.get("/", (req, res) => {
+    // Step 4: Use the proper syntax from knexjs.org to select and return all records from the table
+});
