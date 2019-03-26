3168712d882bf0adf1f86bdb9988800f8a51b281
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-26 14:25:27 -0400

    Configures the knexjs library to connect to './data/lambda.sqlite3' using sqlite3 module

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const helmet = require('helmet');
 
 const knex = require('knex'); // Step 1: Bring in the knexjs library
 
+const knexConfig = {
+  // Step 2: Configure the knexjs library to connect to 
+  // './data/lambda.sqlite3' using sqlite3 module
+  client: "sqlite3", // this is the driver
+  useNullAsDefault: true,
+  connection: {
+    filename: "./data/lambda.sqlite3"
+  },
+  // debug: true
+};
+
 const server = express();
 
 server.use(express.json());
@@ -11,6 +22,6 @@ server.use(helmet());
 // endpoints here
 
 const port = 3300;
-server.listen(port, function() {
+server.listen(port, () => {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
